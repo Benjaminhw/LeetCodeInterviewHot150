898c33375672746d3f1e4880f03931de2fe08957
MZ_Benjamin <ben199709@foxmail.com>
2024-02-20 18:12:21 +0800

    26th&27th

---
+++ 26删除有序数组的重复项/function.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            int j = 1;    //用于比较的临时变量
+            while (i+j<nums.size()&&nums[i + j] == nums[i])    //如果相同，就一直找下去
+            {
+                ++j;
+            }
+            for (int k = 0; k < j-1; ++k)
+            {
+                nums.erase(nums.begin() + i);    //不断地去删除这个位置的元素
+            }
+        }
+        return nums.size();
+    }
+};
+
+//双指针思路
+class SolutionTop {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) {
+            return n;
+        }
+        int left = 1;
+        for (int right = 0; right < n - 1; ++right) {
+            if (nums[right] != nums[right + 1]) {
+                nums[left] = nums[right + 1];
+                left++;
+            }
+        }
+        return left;
+    }
+};
--- LeetInterviewHot100/main.cpp
+++ LeetInterviewHot100/main.cpp
@@ -5,5 +5,14 @@ int main()
 	std::vector<int> a = { 1,2,0,0 };
 	std::vector<int> b = { 1,2 };
 	A.merge(a, 2, b, 2);
+	int x = A.Blue;
 	return 0;
 }
+
+//just A test
+enum Solution::test
+{
+	Red,
+	Green,
+	Blue
+};
