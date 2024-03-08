7a4da4c8ef28a82ac4843804a8ca81fc04a42e50
MZ_Benjamin <ben199709@foxmail.com>
2024-03-08 18:18:51 +0800

    6th

---
+++ 6-Z-ShapeTransform/function.h
@@ -0,0 +1,31 @@
+/*将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+
+比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+
+1P   5A   9H    13N
+2A P 6L S 10I I 14G
+3Y   7I   11R
+之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+
+1P      7I        13N
+2A   6L 8S    12I 14G
+3Y 5A   9H 11R
+4P      10I  
+
+请你实现这个将字符串进行指定行数变换的函数：
+
+string convert(string s, int numRows);
+ */
+
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+	//感觉是一题巧题
+	for()
+};
