704bf4df0ec4c9312300655c94c5d79004d0406a
MZ_Benjamin <ben199709@foxmail.com>
2024-03-12 18:15:23 +0800

    28th

--- 28-FindTheFirstCompareInString/function.h
+++ 28-FindTheFirstCompareInString/function.h
@@ -1,6 +1,8 @@
 /*给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。
 如果 needle 不是 haystack 的一部分，则返回  -1 。*/
 
+//kmp
+
 #pragma once
 #include <iostream>
 #include <vector>
@@ -9,6 +11,8 @@ using namespace std;
 
 
 class Solution {
+private:
+	int next[];
 public:
 	int strStr(string haystack, string needle) {
 		int j = 0, cmp;
