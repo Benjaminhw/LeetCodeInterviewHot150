375238555d1bda1f77a44c4d9898422ede38d7da
MZ_Benjamin <ben199709@foxmail.com>
2024-05-08 18:17:20 +0800

    68

--- 68-WordsAlignment/function.h
+++ 68-WordsAlignment/function.h
@@ -21,7 +21,50 @@ using namespace std;
 
 class Solution {
 public:
-    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+	vector<string> fullJustify(vector<string>& words, int maxWidth) {
+		//It seems that it's not very complex.
+		for (int i = 0; i < words.size(); ++i)
+		{
+			int wordCountWithOneBlank = 0;
+			int leftBlankCount = 0;
+			bool isLastLine = false;
+			//outPut in the same line.
+			vector<string> lineWords;
+			while (wordCountWithOneBlank <= maxWidth)
+			{
+				//If it's already the last line.
+				if (i >= words.size())
+				{
+					//set the flag to true.
+					isLastLine = true;
+				}
 
-    }
+				//If its length doesn't exceed the width.
+				//Plus one is because there has to be a blank between words. 
+				if (wordCountWithOneBlank + words[i].size() + 1 <= maxWidth)
+				{
+					wordCountWithOneBlank += words[i].size() + 1;
+					lineWords.emplace_back(words[i]);
+					++i;
+				}
+
+				//if it exceeds, output the result in this line.
+				else
+				{
+					//Calculate the blank that need to be filled in
+					if (isLastLine)
+					{
+
+					}
+					else
+					{
+						leftBlankCount = maxWidth - wordCountWithOneBlank;
+						break;
+					}
+				}
+			}
+			//words sequence
+			words[i];
+		}
+	}
 };
