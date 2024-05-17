2eab7f8542cc4d3a74916de744e78a91ace054b7
MZ_Benjamin <ben199709@foxmail.com>
2024-05-17 13:52:53 +0800

    68 AC

--- 68-WordsAlignment/function.h
+++ 68-WordsAlignment/function.h
@@ -23,15 +23,17 @@ class Solution {
 public:
 	vector<string> fullJustify(vector<string>& words, int maxWidth) {
 		//It seems that it's not very complex.
+		vector<string> answers;
 		for (int i = 0; i < words.size(); ++i)
 		{
 			int wordCountWithOneBlank = 0;
 			int leftBlankCount = 0;
+			int wordCount = 0;
 			bool isLastLine = false;
 			//outPut in the same line.
 			//
 			vector<string> lineWords;
-			while (wordCountWithOneBlank <= maxWidth)
+			while (wordCountWithOneBlank <= maxWidth && i <= words.size())
 			{
 				//If it's already the last line.
 				if (i >= words.size())
@@ -44,7 +46,7 @@ public:
 				//Making it plus one is because there has to be a blank between words. 
 				//Would last word makes some difference ?? --or the very first word 
 				//If it's the first word
-				if (wordCountWithOneBlank + words[i].size() + 1 <= maxWidth || wordCountWithOneBlank == 0)
+				if (i < words.size() && (wordCountWithOneBlank + words[i].size() + 1 <= maxWidth || wordCountWithOneBlank == 0))
 				{
 					if (wordCountWithOneBlank == 0)
 					{
@@ -54,50 +56,84 @@ public:
 					{
 						wordCountWithOneBlank += words[i].size() + 1;
 					}
+					wordCount += words[i].size();
 					lineWords.emplace_back(words[i]);
 					++i;
 				}
 				//if it exceeds, output the result in this line.
 				else
 				{
+					string answer;
 					//Calculate the blank that need to be filled in
 					if (isLastLine)
 					{
+
+						int tempCount = 0;
 						for (auto& y : lineWords)
 						{
-							cout << y;
+							//cout << y;
+							answer += y;
+							tempCount += y.size();
 							if (y != lineWords.back())
 							{
 								//if not the last word 
-								cout << " ";
+								//cout << " ";
+								answer += " ";
+								++tempCount;
 							}
 						}
+						for (int j = 0; j < maxWidth - tempCount; ++j)
+						{
+							answer += " ";
+						}
+						answers.emplace_back(answer);
+						break;
 					}
 					else
 					{
-						leftBlankCount = maxWidth - wordCountWithOneBlank;
-						int splitBlank = leftBlankCount / lineWords.size();
-						int splitBlankLeft = leftBlankCount % (lineWords.size());
+						leftBlankCount = maxWidth - wordCount;
+						//actually we have to have it minus 1
+						int splitBlank = leftBlankCount;
+						int splitBlankLeft = 0;
+						if (lineWords.size() != 1)
+						{
+							splitBlank = leftBlankCount / (lineWords.size() - 1);
+							splitBlankLeft = leftBlankCount % (lineWords.size() - 1);
+						}
 						for (auto& y : lineWords)
 						{
-							cout << y;
-							cout << " ";
+							//cout << y;
+							answer += y;
+							//cout << " ";
+							if (&y == &lineWords.back() && lineWords.size() != 1)
+							{
+								break;
+							}
 							for (int k = 0; k < splitBlank; ++k)
 							{
-								cout << " ";
+								//cout << " ";
+								answer += " ";
 							}
 							if (splitBlankLeft > 0)
 							{
-								cout << " ";
+								//cout << " ";
+								answer += " ";
 								--splitBlankLeft;
 							}
 						}
-						break;
+						answers.emplace_back(answer);
+						lineWords.clear();
+						wordCountWithOneBlank = 0;
+						wordCount = 0;
+						//cout << endl;
 					}
 				}
 			}
 			//words sequence
 			//words[i];
 		}
+		return answers;
 	}
 };
+
+////All clear!
--- 68-WordsAlignment/main.cpp
+++ 68-WordsAlignment/main.cpp
@@ -3,5 +3,9 @@
 
 int main()
 {
+	vector<string> words;
+	words = { "Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do" };
+	Solution A;
+	A.fullJustify(words, 20);
 	return 0;
 }
