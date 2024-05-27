d4c4ea6f108a7eaa2f61aa40a853ee1fe3ba5cb3
MZ_Benjamin <ben199709@foxmail.com>
2024-05-27 14:38:35 +0800

    226th

---
+++ 226-InvertTree/function.h
@@ -0,0 +1,35 @@
+/*
+* 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点
+*/
+
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//Definition for a binary tree node.
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+	TreeNode* invertTree(TreeNode* root) {
+		if (root == nullptr)
+		{
+			return root;
+		}
+		invertTree(root->left);
+		invertTree(root->right);
+		auto tempPtr = root->left;
+		root->left = root->right;
+		root->right = tempPtr;
+		return root;
+	}
+};
