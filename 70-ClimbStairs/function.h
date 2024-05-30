067787d55d351c7dd42dfcb740bf4fed0f380ed1
Benjamin <ben199709@foxmail.com>
2024-05-30 22:22:21 +0800

    70th

---
+++ 70-ClimbStairs/function.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int climbStairs(int n) {
+
+		//// 35/45 
+		//if (n == 1)
+		//{
+		//	return 1;
+		//}
+		//else if (n == 2)
+		//{
+		//	return 2;
+		//}
+		//return climbStairs(n - 2) + climbStairs(n - 1);
+
+		//可以从1开始加 加到那个地儿
+		int p = 0, q = 0, r = 1;
+		// 第一步的时候是1  p和q存储前两次的结果
+		for (int i = 1; i <= n; ++i)
+		{
+			// 0的时候是泛指1的时候
+			p = q;
+			q = r;
+			r = q + p;
+		}
+	}
+};
