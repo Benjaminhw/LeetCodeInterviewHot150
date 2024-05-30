765992d8c8d8b35b2601e47836e1160cfdfae057
Benjamin <ben199709@foxmail.com>
2024-05-30 21:29:06 +0800

    530th

--- 530-MinimumDifference/function.h
+++ 530-MinimumDifference/function.h
@@ -8,13 +8,46 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits>
 
 using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
 
 
 class Solution {
 public:
-	int getMinimumDifference(TreeNode* root) {
+	int result = INT_MAX;
+	int pre = INT_MIN;
 
+	int getMinimumDifference(TreeNode* root)
+	{
+		if (root == nullptr)
+		{
+			return result;
+		}
+
+		getMinimumDifference(root->left);
+
+		if (pre == INT_MIN)
+		{
+			pre = root->val;
+		}
+		else
+		{
+			result = min(result, pre - root->val);
+			pre = root->val;
+		}
+
+		getMinimumDifference(root->right);
+
+		return result;
 	}
 };
