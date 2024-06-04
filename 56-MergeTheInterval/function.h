b6226faf6dfe931f028d207bd52ced9b70333f12
MZ_Benjamin <ben199709@foxmail.com>
2024-06-04 18:21:01 +0800

    56th

---
+++ 56-MergeTheInterval/function.h
@@ -0,0 +1,57 @@
+/*
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+
+示例 1：
+
+输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+输出：[[1,6],[8,10],[15,18]]
+解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+*/
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+	class cmp {
+	public:
+		bool operator()(vector<int> a, vector<int> b)
+		{
+			return a[0] < b[0];
+		}
+	};
+
+	vector<vector<int>> merge(vector<vector<int>>& intervals) {
+		//interesting
+		if (intervals.size() == 0)
+		{
+			return intervals;
+		}
+		sort(intervals.begin(), intervals.end(), cmp());
+		vector<vector<int>> ans;
+		int i = 1, rangeStart = intervals[0][0], rangeEnd = intervals[0][1];
+		while (i < intervals.size())
+		{
+			while (i < intervals.size() && intervals[i][0] <= intervals[i - 1][1])
+			{
+				rangeEnd = max(rangeEnd, intervals[i][1]);
+				++i;
+			}
+			vector<int> tempVecInt = { rangeStart,rangeEnd };
+			ans.emplace_back(tempVecInt);
+			rangeStart = intervals[i][0];
+			rangeEnd = intervals[i][1];
+			++i;
+		}
+		//last one
+		vector<int> tempVecInt = { rangeStart,rangeEnd };
+		ans.emplace_back(tempVecInt);
+		return ans;
+	}
+};
---
+++ 56-MergeTheInterval/main.cpp
@@ -0,0 +1,10 @@
+#include "function.h"
+
+
+int main()
+{
+	Solution A;
+	vector<vector<int>> x = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+	auto y = A.merge(x);
+	return 0;
+}
