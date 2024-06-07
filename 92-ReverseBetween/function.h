3da21205f44f4eea77e1a40f6a22046725d4071a
Benjamin <ben199709@foxmail.com>
2024-06-08 01:51:00 +0800

    92

--- 92-ReverseBetween/function.h
+++ 92-ReverseBetween/function.h
@@ -20,12 +20,74 @@ class Solution {
 public:
 	ListNode* reverseBetween(ListNode* head, int left, int right) {
 		int i = 1;
+		if (left == right)
+		{
+			return head;
+		}
+		ListNode* OriginalHead = nullptr, * OriginalTail = nullptr, * Pre = nullptr, * Post = nullptr, * OriginalPreHead = nullptr, * OriginalPostTail = nullptr, * theVeryFirstHead = head, * theVeryLastTail = nullptr;
 		while (head != nullptr)
 		{
-			if (i == left)
+			if (head->next == nullptr)
 			{
-
+				theVeryLastTail = head;
 			}
+			if (i == left - 1)
+			{
+				Pre = head;
+				OriginalPreHead = head;
+				head = head->next;
+			}
+			else if (i == left)
+			{
+				OriginalHead = head;
+				Pre = head;
+				head = head->next;
+			}
+			else if (i == right)
+			{
+				Post = head->next;
+				OriginalTail = head;
+				head->next = Pre;
+				head = Post;
+			}
+			else if (i == right + 1)
+			{
+				OriginalPostTail = head;
+				head = head->next;
+			}
+			else if (i > left && i < right)
+			{
+				Post = head->next;
+				head->next = Pre;
+				Pre = head;
+				head = Post;
+			}
+			else
+			{
+				head = head->next;
+			}
+			++i;
 		}
+		if (OriginalPreHead != nullptr)
+		{
+			OriginalPreHead->next = OriginalTail;
+		}
+		else
+		{
+			OriginalHead->next = nullptr;
+		}
+		if (OriginalPostTail != nullptr)
+		{
+			OriginalHead->next = OriginalPostTail;
+		}
+		else
+		{
+			OriginalHead->next = nullptr;
+		}
+		if (OriginalPreHead == nullptr)
+		{
+			return OriginalTail;
+		}
+		return theVeryFirstHead;
 	}
 };
