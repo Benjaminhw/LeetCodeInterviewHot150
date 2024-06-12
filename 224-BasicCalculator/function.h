78b197d13f8be5ebd0777a57aa90c0e4bd3c4d57
Benjamin <ben199709@foxmail.com>
2024-06-12 20:42:18 +0800

    224

--- 224-BasicCalculator/function.h
+++ 224-BasicCalculator/function.h
@@ -1,13 +1,50 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
-
+//只用于应对+-的思路
 class Solution {
 public:
-	int calculate(string s) {
+    int calculate(string s) {
+        stack<int> ops;
+        ops.push(1);
+        int sign = 1;
 
-	}
+        int ret = 0;
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            if (s[i] == ' ') {
+                i++;
+            }
+            else if (s[i] == '+') {
+                sign = ops.top();
+                i++;
+            }
+            else if (s[i] == '-') {
+                sign = -ops.top();
+                i++;
+            }
+            else if (s[i] == '(') {
+                ops.push(sign);
+                i++;
+            }
+            else if (s[i] == ')') {
+                ops.pop();
+                i++;
+            }
+            else {
+                long num = 0;
+                while (i < n && s[i] >= '0' && s[i] <= '9') {
+                    num = num * 10 + s[i] - '0';
+                    i++;
+                }
+                ret += sign * num;
+            }
+        }
+        return ret;
+    }
 };
