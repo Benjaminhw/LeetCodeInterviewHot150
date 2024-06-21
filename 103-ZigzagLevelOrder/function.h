ff8e53c573c274c387a95002347c221260517242
Benjamin <ben199709@foxmail.com>
2024-06-21 22:29:36 +0800

    103

--- 103-ZigzagLevelOrder/function.h
+++ 103-ZigzagLevelOrder/function.h
@@ -4,6 +4,15 @@
 
 using namespace std;
 
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
 //我的评价是reverse秒了 可以直接用上一题
 class Solution {
 public:
